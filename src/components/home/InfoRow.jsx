4686b3e317162030fae1e6be2794f578325dffd1
Manikant Jha <manikantcodes@gmail.com>
2024-02-12 19:10:41 +0530

    worked on the home page website

---
+++ src/components/home/InfoRow.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "../../styles/home/infoRow.module.css";
+
+function InfoRow(props) {
+  if (props.contentFirst) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.descContainer}>
+          <h2>{props.title}</h2>
+          <p>{props.desc}</p>
+          <button>Know More</button>
+        </div>
+        <div className={styles.imgContainer}>
+          <img src={props.src} alt={props.title} />
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <img src={props.src} alt={props.title} />
+      </div>
+      <div className={styles.descContainer}>
+        <h2>{props.title}</h2>
+        <p>{props.desc}</p>
+        <button>Know More</button>
+      </div>
+    </div>
+  );
+}
+
+export default InfoRow;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import Navbar from "../layouts/Navbar";
 import HeroSection from "../components/home/HeroSection";
+import InfoRow from "../components/home/InfoRow";
+import Navbar from "../layouts/Navbar";
 
 function Home() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <InfoRow
+        src="/images/03.jpg"
+        title="About Us"
+        desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          aspernatur reprehenderit possimus adipisci laudantium voluptatibus
+          cumque vel quisquam necessitatibus esse?"
+      />
+      <InfoRow
+        src="/images/04.jpg"
+        title="Our Journey"
+        desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          aspernatur reprehenderit possimus adipisci laudantium voluptatibus
+          cumque vel quisquam necessitatibus esse?"
+        contentFirst={true}
+      />
     </>
   );
 }
