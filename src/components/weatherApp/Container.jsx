f95c7f59cc99137a813ff2d0f690144d5f8143a7
Manikant Jha <manikantcodes@gmail.com>
2024-03-12 15:12:41 +0530

    about to integrate api

--- src/components/weatherApp/Container.jsx
+++ src/components/weatherApp/Container.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import styles from "../../styles/weatherApp/container.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCloudRain,
+  faDroplet,
+  faSearch,
+  faWind,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Container() {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=surat&appid=2927389bf8b98ec157aee52c9c1f5b73"
+  ).then((res) => {
+    console.log(res);
+  });
+
   return (
     <div className={styles.containerMain}>
       <div className={styles.containerApp}>
         <div className={styles.containerAppInnerUpper}>
-          <div className={styles.searchBar}></div>
+          <div className={styles.searchBar}>
+            <input type="text" />
+            <button>
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          </div>
           <div className={styles.tempInfo}>
             <img src="/images/sunny.png" alt="" />
             <div>
@@ -14,7 +32,20 @@ function Container() {
               <p className={styles.weather}>Thunderstorm</p>
             </div>
           </div>
-          <div className={styles.extraInfo}></div>
+          <div className={styles.extraInfo}>
+            <div>
+              <FontAwesomeIcon icon={faWind} />
+              <p>15 km/h</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faDroplet} />
+              <p>15 km/h</p>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faCloudRain} />
+              <p>15 km/h</p>
+            </div>
+          </div>
         </div>
         <div className={styles.containerAppInnerLower}></div>
       </div>
