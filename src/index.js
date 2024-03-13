11f8d3b8ef63f2410abf67fd8ffa0d9b6246edb8
Manikant Jha <manikantcodes@gmail.com>
2024-03-13 17:36:18 +0530

    static-site basic setup

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
 import ReactDOM from "react-dom/client";
-import PriceCard from "./PriceCard";
-
-function title(title) {
-  return <h1>{title}</h1>;
-}
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <>
-    <PriceCard
-      title="Apple"
-      price={5}
-      image="https://5.imimg.com/data5/AK/RA/MY-68428614/apple-500x500.jpg"
-    />
-    <PriceCard
-      title="Banana"
-      price={2}
-      image="https://images.immediate.co.uk/production/volatile/sites/30/2017/01/Bunch-of-bananas-67e91d5.jpg"
-    />
-    <PriceCard
-      title="Cherry"
-      price={10}
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Cherry_season_%2848216568227%29.jpg/640px-Cherry_season_%2848216568227%29.jpg"
-    />
-  </>
-);
+root.render(<App />);
