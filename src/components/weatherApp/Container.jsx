6d4338a09c5b8a303da291d8bc1e18be29ff61de
Manikant Jha <manikantcodes@gmail.com>
2024-03-14 16:11:14 +0530

    integrated api

--- src/components/weatherApp/Container.jsx
+++ src/components/weatherApp/Container.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/weatherApp/container.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -7,38 +7,77 @@ import {
   faSearch,
   faWind,
 } from "@fortawesome/free-solid-svg-icons";
-import { getWeatherData } from "../../services/apiServices";
+import {
+  getMultiDayWeatherData,
+  getWeatherData,
+} from "../../services/apiServices";
+
+function kelvinToCelcius(temperature) {
+  if (!temperature) return 0;
+  return Math.round(temperature - 273.15);
+}
 
 function Container() {
-  const data = getWeatherData("delhi");
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [multiDayData, setMultiDayData] = useState(null);
 
-  console.log("data", data);
+  function handleChange(e) {
+    setCity(e.target.value);
+  }
+
+  function handleSearch() {
+    getWeatherData(city)
+      .then((data) => {
+        setWeather(data);
+        getMultiDayWeatherData(data.coord.lat, data.coord.lon, 4)
+          .then((data) => {
+            setMultiDayData(data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  console.log("weather", weather);
+  console.log("multiDayData", multiDayData);
 
   return (
     <div className={styles.containerMain}>
       <div className={styles.containerApp}>
         <div className={styles.containerAppInnerUpper}>
           <div className={styles.searchBar}>
-            <input type="text" />
-            <button>
+            <input type="text" value={city} onChange={handleChange} />
+            <button onClick={handleSearch}>
               <FontAwesomeIcon icon={faSearch} />
             </button>
           </div>
           <div className={styles.tempInfo}>
-            <img src="/images/sunny.png" alt="" />
+            <img
+              src={`/images/${
+                weather?.weather[0]?.icon.slice(0, 2) + "d" || "01d"
+              }.png`}
+              alt=""
+            />
             <div>
-              <p className={styles.temperature}>23°</p>
-              <p className={styles.weather}>Thunderstorm</p>
+              <p className={styles.temperature}>
+                {kelvinToCelcius(weather?.main?.temp)}°
+              </p>
+              <p className={styles.weather}>{weather?.weather[0]?.main}</p>
             </div>
           </div>
           <div className={styles.extraInfo}>
             <div>
               <FontAwesomeIcon icon={faWind} />
-              <p>15 km/h</p>
+              <p>{weather?.wind?.speed} km/h</p>
             </div>
             <div>
               <FontAwesomeIcon icon={faDroplet} />
-              <p>15 km/h</p>
+              <p>{weather?.main?.humidity}</p>
             </div>
             <div>
               <FontAwesomeIcon icon={faCloudRain} />
--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -1,41 +1,37 @@
-// const response = fetch(
-//   "https://api.openweathermap.org/data/2.5/weather?q=surat&appid=2927389bf8b98ec157aee52c9c1f5b73"
-// )
-//   .then((response) => {
-//     console.log("response a", response);
-//     return response.json();
-//   })
-//   .catch((error) => {
-//     console.log("error", error);
-//   });
-
-// response
-//   .then((data) => {
-//     console.log("data", data);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-// console.log("response", response);
-
 function getWeatherData(city) {
-  let weather = null;
-
-  fetch(
+  const promise = fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_OWA_KEY}`
   )
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      return data;
     })
     .catch((error) => {
       console.log("error", error);
     });
 
-  return weather;
+  return promise;
 }
 
-export { getWeatherData };
+function getMultiDayWeatherData(lat, lon, cnt) {
+  console.log("process.env.REACT_APP_OWA_KEY", process.env.REACT_APP_OWA_KEY);
+  const promise = fetch(
+    `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_OWA_KEY}`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log("data", data);
+      return data;
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+
+  return promise;
+}
+
+export { getWeatherData, getMultiDayWeatherData };
