03cc48b1ab665cea2ad2e3bc970ea6e27eb32f76
Manikant Jha <manikantcodes@gmail.com>
2024-04-16 18:10:00 +0530

    made navbar responsive in mobile

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,25 +1,36 @@
-import React from "react";
-import "../styles/home/navbar.css";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "../styles/home/navbar.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleOpen() {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <nav className="navContainer">
       <h1 className="logo">Purple Bit</h1>
-      <ul className="links">
-        <li>
+      <ul className={`links ${isOpen ? "open" : ""}`}>
+        <li onClick={toggleOpen}>
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li onClick={toggleOpen}>
           <Link to="/about">About</Link>
         </li>
-        <li>
+        <li onClick={toggleOpen}>
           <Link to="/portfolio">Portfolio</Link>
         </li>
-        <li>
+        <li onClick={toggleOpen}>
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
+      <button className="btnMenu" onClick={toggleOpen}>
+        <FontAwesomeIcon icon={faBars} />
+      </button>
     </nav>
   );
 }
