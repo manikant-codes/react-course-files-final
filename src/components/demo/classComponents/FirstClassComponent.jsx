343e5254f23ff754779ea2bb8c3621c5d02dcead
Manikant Jha <manikantcodes@gmail.com>
2024-05-02 14:03:44 +0530

    lifecycle methods

--- src/components/demo/classComponents/FirstClassComponent.jsx
+++ src/components/demo/classComponents/FirstClassComponent.jsx
@@ -1,9 +1,49 @@
 import React, { Component } from "react";
+import FirstFunctionComponent from "../functionComponents/FirstFunctionComponent";
 
 class FirstClassComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { name: "Manikant" };
+    this.state = { name: "Manikant", color: "", users: null, posts: null };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    return { color: props.color };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        this.setState({ users: data });
+      });
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  // if (this.props.color === nextProps.color) {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+  // }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    return prevState.posts;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("componentDidUpdate");
+    if (snapshot === null) {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          this.setState({ posts: data });
+        });
+    }
   }
 
   changeName = () => {
@@ -12,11 +52,16 @@ class FirstClassComponent extends Component {
   };
 
   render() {
+    console.log("users", this.state.users);
     return (
       <div>
         <p>Hello, {this.state.name}</p>
-        <p>Color, {this.props.color}</p>
+        <p>Color, {this.state.color}</p>
         <button onClick={this.changeName}>Change Msg</button>
+        <FirstFunctionComponent />
+        {this.state.posts?.map((value) => {
+          return <p>{value.title}</p>;
+        })}
       </div>
     );
   }
