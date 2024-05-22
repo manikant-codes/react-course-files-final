3154070c54dfd767f30747b63b121a1164ac4cf5
Manikant Jha <manikantcodes@gmail.com>
2024-05-22 10:43:28 +0530

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Layout from "./layouts/Layout";
-import RecipeDetails from "./pages/RecipeDetails";
-import { useEffect, useState } from "react";
-import { getRandomRecepies } from "./services/apiServices";
-
 function App() {
-  const [recipes, setRecipes] = useState(null);
-
-  useEffect(() => {
-    getRandomRecepies().then((result) => {
-      setRecipes(result?.recipes);
-    });
-  }, []);
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout setRecipes={setRecipes} />}>
-          <Route index element={<Home recipes={recipes} />} />
-          <Route path="details/:kuchbhi" element={<RecipeDetails />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1>Hello World!</h1>;
 }
 
 export default App;
