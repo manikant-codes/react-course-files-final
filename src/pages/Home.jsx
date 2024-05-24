99b1bc389f655885bc85618cbe6510b81d22c27f
Manikant Jha <manikantcodes@gmail.com>
2024-05-24 18:10:12 +0530

    taught tailwind and react flowbite

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,38 @@
+import { Button } from "flowbite-react";
 import React from "react";
+import { HiOutlineArrowRight, HiShoppingCart } from "react-icons/hi";
 
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    // <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    //   <h1 className="text-4xl font-bold text-[#a5d6a7] bg-purple-500 p-[32px] [&:hover]:bg-purple-800 [&:hover]:text-[#ff9800] rounded-xl">
+    //     Home
+    //   </h1>
+    //   <h1 className="text-4xl font-bold text-[#a5d6a7] bg-purple-500 p-[32px] [&:hover]:bg-purple-800 [&:hover]:text-[#ff9800]">
+    //     Home
+    //   </h1>
+    //   <h1 className="text-4xl font-bold text-[#a5d6a7] bg-purple-500 p-[32px] [&:hover]:bg-purple-800 [&:hover]:text-[#ff9800]">
+    //     Home
+    //   </h1>
+    //   <h1
+    //     className={`text-4xl font-bold text-[#a5d6a7] bg-purple-500 p-[32px] [&:hover]:bg-purple-800 [&:hover]:text-[#ff9800]`}
+    //   >
+    //     Home
+    //   </h1>
+    // </div>
+
+    <Button
+      className="m-8"
+      // size="xs"
+      outline
+      gradientDuoTone="purpleToBlue"
+      pill
+      isProcessing
+    >
+      Default
+      {/* <HiOutlineArrowRight className="h-4 w-4" /> */}
+    </Button>
+  );
 }
 
 export default Home;
