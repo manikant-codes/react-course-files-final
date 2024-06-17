79c7faf8b99e538d112d8f844b694c28a7d997af
Manikant Jha <being.manikantjha@gmail.com>
2024-06-17 18:04:49 +0530

    completed lifecycle methods

--- src/components/AnotherClassComponent.jsx
+++ src/components/AnotherClassComponent.jsx
@@ -1,6 +1,9 @@
 import { Component } from "react";
 
 class AnotherClassComponent extends Component {
+  componentWillUnmount() {
+    console.log("Unmounting");
+  }
   render() {
     return <h1>Hello!</h1>;
   }
--- src/components/ClassComponetsDemoI.jsx
+++ src/components/ClassComponetsDemoI.jsx
@@ -1,3 +1,4 @@
+import { isVisible } from "@testing-library/user-event/dist/utils";
 import AnotherClassComponent from "./AnotherClassComponent";
 
 const { Component } = require("react");
@@ -5,7 +6,13 @@ const { Component } = require("react");
 class MyFirstClassComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { count: 0, name: "", users: null };
+    this.state = {
+      count: 0,
+      name: "",
+      users: null,
+      posts: null,
+      isVisible: true,
+    };
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -20,6 +27,38 @@ class MyFirstClassComponent extends Component {
     this.setState({ count: this.state.count - 1 });
   };
 
+  // shouldComponentUpdate(nextProps, nextState) {
+  // console.log("this.state", this.state);
+  // console.log("nextState", nextState);
+  // if (this.state.count !== nextState.count) {
+  //   return true;
+  // }
+  // return false;
+  // }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    if (prevState.count >= 5) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (snapshot === true && this.state.posts === null) {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => {
+          const json = res.json();
+          return json;
+        })
+        .then((data) => {
+          console.log("data", data);
+          this.setState({ posts: data });
+        })
+        .catch(() => {});
+    }
+  }
+
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
@@ -33,15 +72,21 @@ class MyFirstClassComponent extends Component {
       .catch(() => {});
   }
 
+  toggleVisible = () => {
+    this.setState({ isVisible: !this.state.isVisible });
+  };
+
   render() {
     console.log("users", this.state.users);
     return (
       <div>
-        <AnotherClassComponent />
-        <h1>Hello {this.state.name}!</h1>
+        {this.state.isVisible && <AnotherClassComponent />}
+        <button onClick={this.toggleVisible}>Show</button>
+        {/* <h1>Hello {this.state.name}!</h1>
         <button onClick={this.decrement}>-</button>
         {this.state.count}
         <button onClick={this.increment}>+</button>
+        {this.state.users && this.state.users[0]?.name} */}
       </div>
     );
   }
